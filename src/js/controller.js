531caa626badec67eb97494ce3d8681b3b508b34
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-05-07 16:50:25 +0200

    feat: add recipes rendering and recipe viewing (fix) recipe hash and "empty" reserch

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,17 @@
 import * as model from './model.js';
 import searchView from './views/searchView.js';
 import recipeView from './views/recipeView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
-//! Forkify Recipes Web Sites:  https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+//! Forkify Recipes Web Sites:  https://forkify-api.herokuapp.com/v2
+//parcel..
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -29,6 +33,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) get search query
     const query = searchView.getQuery();
 
@@ -38,7 +44,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3)"render" resaults
-    console.log(model.state.search.results);
+    /*  console.log(model.state.search.results); */
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
