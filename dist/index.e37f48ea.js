3742f5ef92fe310641264b1c4c5a354a135fbce2
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-05-08 11:42:18 +0200

    feat: add localStorage to bookmarks - step 1

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2592,11 +2592,16 @@ const updateServings = function(newServings) {
     });
     state.recipe.servings = newServings;
 };
+//* store system for bookmarks
+const persistBookmarks = function() {
+    localStorage.setItem("bookmarks", JSON.stringify(state.bookmarks));
+};
 const addBookmark = function(recipe) {
     // Add bookmark
     state.bookmarks.push(recipe);
     // Mark current recipe as bookmark
     if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+    persistBookmarks();
 };
 const deleteBookmark = function(id) {
     //delete bookmark
@@ -2604,6 +2609,7 @@ const deleteBookmark = function(id) {
     state.bookmarks.splice(index, 1);
     //set bookmark to false (Not Bookmarked)
     if (id === state.recipe.id) state.recipe.bookmarked = false;
+    persistBookmarks();
 };
 
 },{"regenerator-runtime":"dXNgZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./config.js":"k5Hzs","./helpers.js":"hGI1E"}],"k5Hzs":[function(require,module,exports) {
--- src/js/model.js
+++ src/js/model.js
@@ -77,13 +77,18 @@ export const updateServings = function (newServings) {
 
   state.recipe.servings = newServings;
 };
-
+//* store system for bookmarks
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmark
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persistBookmarks();
 };
 
 export const deleteBookmark = function (id) {
@@ -92,4 +97,6 @@ export const deleteBookmark = function (id) {
   state.bookmarks.splice(index, 1);
   //set bookmark to false (Not Bookmarked)
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  persistBookmarks();
 };
